a276e98567060a1dbe999a3e0ca09dd5be785e78
pzeder <52744148+pzeder@users.noreply.github.com>
2025-05-04 02:09:18 +0200

    Implement game logic

--- angular-project/src/app/core/game.service.ts
+++ angular-project/src/app/core/game.service.ts
@@ -1,10 +1,52 @@
-import { Injectable } from '@angular/core';
+import {computed, effect, Injectable, Signal, signal} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  private test = 'test123';
+  board = signal(this.getEmptyBoard());
+  currentPlayer = signal<'X' | 'O'>('X');
 
-  constructor() { }
+  gameOver = computed(() =>
+    this.winner() !== '' || !this.board().map(r => r.some(x => x === '')).some(x => x));
+
+  winner: Signal<'X' | 'O' | ''>= computed(() => {
+    const board = this.board();
+    const rowWinners: ('X' | 'O' | '')[] = [
+      this.getWinnerFromRow(board[0]),
+      this.getWinnerFromRow(board[1]),
+      this.getWinnerFromRow(board[2]),
+      this.getWinnerFromRow(board.map(r => r[0])),
+      this.getWinnerFromRow(board.map(r => r[1])),
+      this.getWinnerFromRow(board.map(r => r[2])),
+      this.getWinnerFromRow([board[0][0],board[1][1],board[2][2]]),
+      this.getWinnerFromRow([board[0][2],board[1][1],board[2][0]]),
+    ];
+    return rowWinners.filter(w => w !== '')[0] ?? '';
+  })
+
+  tryPlay(row: number, column: number) {
+    const board = this.board();
+    if (board[row][column] !== '') {
+      return;
+    }
+
+    board[row][column] = this.currentPlayer();
+    this.board.set([...board]);
+    const nextPlayer = this.currentPlayer() === 'X' ? 'O' : 'X';
+    this.currentPlayer.set(nextPlayer);
+  }
+
+  reset() {
+    this.board.set(this.getEmptyBoard());
+    this.currentPlayer.set('X');
+  }
+
+  private getWinnerFromRow(row: ('X' | 'O' | '')[]) {
+    return row.every(c => c === row[0]) ? row[0] : '';
+  }
+
+  private getEmptyBoard(): ('X' | 'O' | '')[][] {
+    return [['', '', ''],['', '', ''],['', '', '']];
+  }
 }
